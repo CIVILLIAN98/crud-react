0585838ecb994b1221bda1d622f462b589aab159
CIVILLIAN98 <civillian1998@gmail.com>
2022-12-21 22:47:16 +0900

    delete and search added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { styles } from "./App.css";
+import "./App.css";
 import data from "./App.test";
 
 export default class App extends Component {
@@ -7,29 +7,64 @@ export default class App extends Component {
     super(props);
     this.state = {
       task: data,
+      selected: "name",
     };
   }
   render() {
+    const OnDelete = (id) => {
+      console.log(id, "id");
+      const res = this.state.task.filter((val) => val.id !== id);
+      this.setState({ task: res });
+    };
+    const onSearch = (e) => {
+      const search = data.filter((val) =>
+        val[this.state.selected]
+          .toLocaleLowerCase()
+          .includes(e.target.value.toLocaleLowerCase())
+      );
+      this.setState({ task: search });
+    };
+    const onSelect = (e) => {
+      this.setState({ selected: e.target.value });
+    };
     return (
       <div className="container">
         <h1>
           <p className="header">CRUD PROJECT IN REACT JS BY CIVILLIAN_98</p>
         </h1>
+        <input
+          className="search"
+          placeholder="Search"
+          type="text"
+          onChange={onSearch}
+        />
+        <select onChange={onSelect} name="" id="">
+          <option value="name">NAME</option>
+          <option value="status">STATUS</option>
+        </select>
         <table className="table" border={1}>
           <thead>
             <tr>
               <th>ID</th>
               <th>NAME</th>
               <th>STATUS</th>
+              <th>UPDATE</th>
+              <th>DELETE</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.task.map((val) => {
+            {this.state.task.map((val, index) => {
               return (
                 <tr key={val.id}>
-                  <td>{val.id}</td>
+                  <td>{index + 1}</td>
                   <td>{val.name}</td>
                   <td>{val.status}</td>
+                  <td>
+                    <button>UPDATE</button>
+                  </td>
+                  <td>
+                    <button onClick={() => OnDelete(val.id)}>DELETE</button>
+                  </td>
                 </tr>
               );
             })}
